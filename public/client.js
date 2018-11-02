549da3d1c440e08912b915884ccea744a20fd4b4
PantherPat <Patrick.keshishian@gmail.com>
2018-11-02 14:00:29 -0400

    Most html and css started javascript

--- public/client.js
+++ public/client.js
@@ -4,7 +4,34 @@
 
 //use global functions, objects, and variables (triggers)
 
-var signUp = "".val();
-var userName = "".val();
-var password = "".val();
-var searchBar = "".val();
+//when the page loads...
+$(document).ready(function () {
+    $("main").hide();
+    $(".signUpForm").hide();
+    $("#signInPageWrapper").show();
+
+
+});
+
+//button triggers
+$(document).on('click', '.showRegister', function (event) {
+    event.preventDefault();
+    $("main").hide();
+    $(".signUpForm").show();
+    $("#signInPageWrapper").show();
+    $("#signInPage").hide();
+});
+
+$(document).on('click', '.showSignIn', function (event) {
+    event.preventDefault();
+    $("main").hide();
+    $(".signUpForm").hide();
+    $("#signInPageWrapper").show();
+    $("#signInPage").show();
+});
+
+
+//form trigger
+$(document).submit('form', function (event) {
+    event.preventDefault();
+});
